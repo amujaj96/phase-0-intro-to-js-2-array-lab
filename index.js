1eba3c67a578d31113b2591b91a54862e52ab91d
Agim <dawnhillsnyc@gmail.com>
2022-10-02 18:45:20 -0400

    commit tree 71bb577d79e44472a4f370b718490eecdf1d9950

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const copyCat = [...cats, name]
+    return copyCat
+}
+
+function prependCat(name) {
+    const secondCopyCat = [name, ...cats]
+    return secondCopyCat
+}
+
+function removeLastCat() {
+   return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
